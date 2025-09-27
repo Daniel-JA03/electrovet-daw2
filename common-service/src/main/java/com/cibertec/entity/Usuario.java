c5538c4adbe76a2f19244b528eee9fbb3b1ba466
Daniel-JA03 <danieljaimesamancio@gmail.com>
2025-09-26 20:59:15 -0500

    feat(common-service): se agrego las entidades Usuario y Rol con JPA y Lombok

--- common-service/src/main/java/com/cibertec/entity/Usuario.java
+++ common-service/src/main/java/com/cibertec/entity/Usuario.java
@@ -1,4 +1,38 @@
 package com.cibertec.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String apellido;
+
+    @Column(name = "numero_documento")
+    private String numeroDocumento;
+    private String telefono;
+    private String direccion;
+    private String correo;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_rol")
+    private Rol rol;
 }
